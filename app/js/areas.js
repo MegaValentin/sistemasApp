64a85a80cf9e4a25708cc18700e826f1dbf49231
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2023-10-30 12:05:19 -0300

    json

--- app/js/areas.js
+++ app/js/areas.js
@@ -1,39 +1,43 @@
-export function areas(){
-    console.log("control de areas")
+export function areas() {
+  console.log("control de areas");
 
-    const areas = [
-        {id:1,area:"area1"},
-        {id:2,area:"area1"},
-        {id:3,area:"area1"},
-        {id:4,area:"area1"},
-        {id:5,area:"area1"},
-        {id:6,area:"area1"},
-        {id:7,area:"area1"},
-        {id:8,area:"area1"},
-        {id:9,area:"area1"},
-        {id:10,area:"area1"},
-        {id:11,area:"area1"},
-        {id:12,area:"area1"},
-        {id:13,area:"area1"},
-        {id:14,area:"area1"},
-        {id:15,area:"area1"},
-        {id:16,area:"area1"},
-        {id:17,area:"area1"},
-        {id:18,area:"area1"},
-        {id:19,area:"area1"},
-        {id:20,area:"area1"},
-    ]
+  const areas = [
+    { id: 1, area: "area1" },
+    { id: 2, area: "area1" },
+    { id: 3, area: "area1" },
+    { id: 4, area: "area1" },
+    { id: 5, area: "area1" },
+    { id: 6, area: "area1" },
+    { id: 7, area: "area1" },
+    { id: 8, area: "area1" },
+    { id: 9, area: "area1" },
+    { id: 10, area: "area1" },
+    { id: 11, area: "area1" },
+    { id: 12, area: "area1" },
+    { id: 13, area: "area1" },
+    { id: 14, area: "area1" },
+    { id: 15, area: "area1" },
+    { id: 16, area: "area1" },
+    { id: 17, area: "area1" },
+    { id: 18, area: "area1" },
+    { id: 19, area: "area1" },
+    { id: 20, area: "area1" },
+  ];
 
-    const tableOfficeContainer = document.getElementById('tableOfficeContainer');
-    const addOfficeFormContainer = document.getElementById('addOfficeFormContainer')
-    const editOfficeFormContainer = document.getElementById('editOfficeFormContainer')
+  const tableOfficeContainer = document.getElementById("tableOfficeContainer");
+  const addOfficeFormContainer = document.getElementById(
+    "addOfficeFormContainer"
+  );
+  const editOfficeFormContainer = document.getElementById(
+    "editOfficeFormContainer"
+  );
 
-    function actualizarTablaArea() {
-        const tbody = document.querySelector('tbody');
-        tbody.innerHTML = '';
-    
-        for (const area of areas) {
-            tbody.innerHTML += `
+  function actualizarTablaArea() {
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+
+    for (const area of areas) {
+      tbody.innerHTML += `
                 <tr>
                     <td>${area.id}</td>
                     <td>${area.area}</td>
@@ -43,46 +47,43 @@ export function areas(){
                     </td>
                 </tr>
             `;
-        }
-        
-        asignarEventListeners() 
     }
 
-    let formOfficeHTML = `
+    asignarEventListeners();
+  }
+
+  let formOfficeHTML = `
     <h3>Agregar Area</h3>
     <form action="#" id="addOffice" class="addOffice">
         <input type="text" placeholder="Area" id="office" required/>
         <input type="submit" value="+" id="pedido"/>
     </form>
     `;
-    addOfficeFormContainer.innerHTML = formOfficeHTML; 
-    addOffice.addEventListener("submit", (event) =>{
-        event.preventDefault();
-        agregarArea();
-    })
-    const office = document.querySelector('#office');
+  addOfficeFormContainer.innerHTML = formOfficeHTML;
+  addOffice.addEventListener("submit", (event) => {
+    event.preventDefault();
+    agregarArea();
+  });
+  const office = document.querySelector("#office");
 
-    function agregarArea(){
-        let selectedOffice = office.value;
-        let ultimaId
-        for(const area of areas){
-            ultimaId = area.id
-        }
-        ultimaId++
-        const addOffices = {id:ultimaId, area:selectedOffice}
-        
-        areas.push(addOffices)
-
-        office.value = ""
-
-        console.log(areas)
-        actualizarTablaArea()
-        asignarEventListeners()
-
-        
+  function agregarArea() {
+    let selectedOffice = office.value;
+    let ultimaId;
+    for (const area of areas) {
+      ultimaId = area.id;
     }
-    let tableHTML = 
-    `
+    ultimaId++;
+    const addOffices = { id: ultimaId, area: selectedOffice };
+
+    areas.push(addOffices);
+
+    office.value = "";
+
+    console.log(areas);
+    actualizarTablaArea();
+    asignarEventListeners();
+  }
+  let tableHTML = `
     <table>
         <thead>
             <tr>
@@ -94,8 +95,8 @@ export function areas(){
         </thead>
         <tbody>
     `;
-    for (const area of areas) {
-        tableHTML += `
+  for (const area of areas) {
+    tableHTML += `
             <tr>
                 <td>${area.id}</td>
                 <td>${area.area}</td>
@@ -105,79 +106,74 @@ export function areas(){
                 </td>
             </tr>
         `;
-    }
-    tableHTML += 
-    `
+  }
+  tableHTML += `
         </tbody>
     </table>
     `;
-    tableOfficeContainer.innerHTML = tableHTML;
+  tableOfficeContainer.innerHTML = tableHTML;
 
-    function asignarEventListeners() {
-        const editButtons = document.querySelectorAll('.editBtn');
-        editButtons.forEach((button) => {
-            button.addEventListener('click', (event) => {
-                const officeId = event.target.getAttribute('data-id');
-                
+  function asignarEventListeners() {
+    const editButtons = document.querySelectorAll(".editBtn");
+    editButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const officeId = event.target.getAttribute("data-id");
 
-                abrirVistaModificacionArea(officeId);
-            });
-        });
+        abrirVistaModificacionArea(officeId);
+      });
+    });
 
-        const deleteButtons = document.querySelectorAll('.deleteBtn');
-        deleteButtons.forEach((button) => {
-            button.addEventListener('click', (event) => {
-                const officeId = event.target.getAttribute('data-id');
-                eliminarArea(officeId);
-            });
-        });
-    } 
-    function abrirVistaModificacionArea(officeId){
-        let editFormOfficeHTML = 
-        `
+    const deleteButtons = document.querySelectorAll(".deleteBtn");
+    deleteButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const officeId = event.target.getAttribute("data-id");
+        eliminarArea(officeId);
+      });
+    });
+  }
+  function abrirVistaModificacionArea(officeId) {
+    let editFormOfficeHTML = `
             <h3>Editar Area</h3>
             <form action="#" id="editOffice" class="editOffice">
                 <input type="text" placeholder="Area" id="editArea" required/>
                 <input type="submit" value="Editar" id="editar"/>
             </form>
         `;
-    
+
+    editOfficeFormContainer.innerHTML = editFormOfficeHTML;
+
+    const areaIndex = areas.findIndex((area) => area.id == officeId);
+    console.log(`Editar ${officeId}`);
+    if (areaIndex !== -1) {
+      editOffice.addEventListener("submit", (event) => {
+        event.preventDefault();
+        editarArea(areaIndex);
+        let editFormOfficeHTML = ``;
         editOfficeFormContainer.innerHTML = editFormOfficeHTML;
-
-        const areaIndex = areas.findIndex(area => area.id == officeId);
-       console.log(`Editar ${officeId}`);
-        if (areaIndex !== -1){
-            editOffice.addEventListener("submit", (event) =>{
-                event.preventDefault();
-                editarArea(areaIndex);
-                let editFormOfficeHTML = ``;
-                editOfficeFormContainer.innerHTML = editFormOfficeHTML;
-            })
-        }
-        else{
-            console.log(`Area con ID ${officeId} no encontrado`)
-        }
+      });
+    } else {
+      console.log(`Area con ID ${officeId} no encontrado`);
     }
-    function editarArea(areaIndex){
-        const editArea = document.querySelector('#editArea')
+  }
+  function editarArea(areaIndex) {
+    const editArea = document.querySelector("#editArea");
 
-        areas[areaIndex].area = editArea.value;
+    areas[areaIndex].area = editArea.value;
 
-        console.log(`Toner editado: ${JSON.stringify(areas[areaIndex])}`);
+    console.log(`Toner editado: ${JSON.stringify(areas[areaIndex])}`);
 
-        actualizarTablaArea()
-        
+    actualizarTablaArea();
+  }
+
+  function eliminarArea(officeId) {
+    const areaIndex = areas.findIndex((area) => area.id == officeId);
+    console.log(`Eliminar ${officeId}`);
+    if (areaIndex !== -1) {
+      areas.splice(areaIndex, 1);
+
+      actualizarTablaArea();
+      asignarEventListeners();
     }
-    
-    function eliminarArea(officeId){
-        const areaIndex = areas.findIndex(area => area.id == officeId);
-       console.log(`Eliminar ${officeId}`);
-        if (areaIndex !== -1) {
-            areas.splice(areaIndex, 1);
-
-            actualizarTablaArea()
-            asignarEventListeners();
-        }
-    }
-    asignarEventListeners()
+  }
+  asignarEventListeners();
 }
