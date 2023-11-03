451c6f14df0859bc2b35d2b3b05ca177393e05bf
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2023-11-03 13:58:40 -0300

    boton de eleminar

--- app/js/pruebadestock.js
+++ app/js/pruebadestock.js
@@ -1,13 +1,21 @@
-export function prueba(){
-    const stock = './app/data/stock.json'
+export function prueba() {
+    const stock = './app/data/stock.json';
 
-    cargarDatos(stock)
+    cargarDatos(stock);
+
+    
+    let toners = [];
+
     function cargarDatos(url) {
-        fetch(url).then(res => res.json()).then(dato => {
-            console.log(dato.toners)
-            mostrarTabla(dato.toners)
-        })
+        fetch(url)
+            .then(res => res.json())
+            .then(dato => {
+                
+                toners = dato.toners;
+                mostrarTabla(toners);
+            });
     }
+
     function mostrarTabla(toners) {
         const tableContainer = document.getElementById('table-container');
         let tableHTML = `
@@ -23,65 +31,53 @@ export function prueba(){
                 </thead>
                 <tbody>
         `;
-    
+
         for (const toner of toners) {
             tableHTML += `
                 <tr>
                     <td>${toner.id}</td>
                     <td>${toner.toner}</td>
                     <td>${toner.cantidad}</td>
-                    <td >
+                    <td>
                         <button class="editBtn" data-id="${toner.id}" data-toner="${toner.toner}" data-stock="${toner.cantidad}">Editar</button>
                         <button class="deleteBtn" data-id="${toner.id}">Eliminar</button>
                     </td>
                 </tr>
             `;
         }
-    
+
         tableHTML += `
                 </tbody>
             </table>
         `;
-    
-        tableContainer.innerHTML = tableHTML;
-        asignarEventListeners()
-    }
-    function asignarEventListeners() {
-        const editButtons = document.querySelectorAll('.editBtn');
-        editButtons.forEach((button) => {
-            button.addEventListener('click', (event) => {
-                const tonerId = event.target.getAttribute('data-id');
-                const tonerToner = event.target.getAttribute('data-toner')
-                const tonerStock = event.target.getAttribute('data-stock')
 
-                abrirVistaModificacion(tonerId, tonerToner, tonerStock);
-            });
-        });
+        tableContainer.innerHTML = tableHTML;
+        
+        const editBtn = document.querySelectorAll('.editBtn');
+        editBtn.forEach(editBtn => {
+            editBtn.addEventListener('click', () => editarToner(toners, editBtn));
+        })
 
         const deleteButtons = document.querySelectorAll('.deleteBtn');
-        deleteButtons.forEach((button) => {
-            button.addEventListener('click', (event) => {
-                const tonerId = event.target.getAttribute('data-id');
-                eliminarToner(tonerId);
-            });
+        deleteButtons.forEach(deleteBtn => {
+            deleteBtn.addEventListener('click', () => eliminarToner(toners, deleteBtn));
         });
-       
     }
-    function eliminarToner(tonerId) {    
-        
 
-    // Busca el índice del toner con el ID correspondiente en la lista de toners
+    function eliminarToner(toners, button) {
+        const tonerId = button.getAttribute('data-id');
+
         const tonerIndex = toners.findIndex(toner => toner.id === parseInt(tonerId));
 
         if (tonerIndex !== -1) {
-            // Elimina el toner de la lista
             toners.splice(tonerIndex, 1);
-            // Actualiza la tabla
             mostrarTabla(toners);
         } else {
-            console.log(`Toner con ID ${tonerId} no encontrado.`);
+            console.log(`Toner with ID ${tonerId} not found.`);
         }
-        }
-    
-        asignarEventListeners()
+    }
+    function editarToner(toner, button){
+        const editFormContainer = document.getElementById('editFormContainer')
+        
+    }
 }
