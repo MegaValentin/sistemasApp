88224b6cbf20345f613849edfa72ad4d5bdd0ea1
MegaValentin <91918490+MegaValentin@users.noreply.github.com>
2023-11-21 12:40:27 -0300

    pedido de toners

--- app/js/pedidoToner.js
+++ app/js/pedidoToner.js
@@ -2,7 +2,36 @@ export function pedidoToner() {
 
     const listaStock = './app/data/stock.json';
     const listaAreas = './app/data/areas.json';
+    const historialEntregas = [];
 
+    async function cargarOptions(url, select, propertyName) {
+        const data = await fetch(url)
+            .then(res => res.json())
+            .catch(error => console.error('Error al cargar opciones:', error));
+
+        if (data && Array.isArray(data[propertyName])) {
+            data[propertyName].forEach(item => {
+                const option = document.createElement('option');
+                option.value = item.id;
+                option.text = item.area || item.toner; 
+                select.add(option);
+            });
+        } else {
+            console.error('La propiedad "' + propertyName + '" no es un array en los datos:', data);
+        }
+    }
+
+    async function obtenerNombreSeleccionado(url, selectedId, propertyName) {
+        const data = await fetch(url)
+            .then(res => res.json())
+            .catch(error => {
+                console.error('Error al obtener el nombre del elemento seleccionado:', error);
+                return 'Error';
+            });
+
+        const selectedItem = data[propertyName].find(item => item.id == selectedId);
+        return selectedItem ? selectedItem.area || selectedItem.toner : 'No encontrado';
+    }
     const formulario = document.getElementById('formToner')
     const formHTML = `
 
@@ -49,42 +78,18 @@ export function pedidoToner() {
         const selectedAreaName = await obtenerNombreSeleccionado(listaAreas, selectedAreaId, 'areas');
         const selectedTonerName = await obtenerNombreSeleccionado(listaStock, selectedTonerId, 'toners');
 
-        const entrega = []
-        entrega.push({"AREA":selectedAreaName,"TONER":selectedTonerName,"Cantidad":cantidad})
-        console.log(entrega);
-        await mensajeEntrega(`Se entrego un ${selectedTonerName} para ${selectedAreaName}`)
+        const entrega = {"AREA": selectedAreaName, "TONER": selectedTonerName, "Cantidad": cantidad};
+       
+        historialEntregas.push(entrega)
+        console.log(historialEntregas);
+        if(cantidad > 1){
+            await mensajeEntrega(`Se entregaron ${cantidad} (${selectedTonerName}) para ${selectedAreaName}`)
+        }else{
+            await mensajeEntrega(`Se entregó un ${selectedTonerName} para ${selectedAreaName}`)
+        }
         
     }
-
-    async function cargarOptions(url, select, propertyName) {
-        const data = await fetch(url)
-            .then(res => res.json())
-            .catch(error => console.error('Error al cargar opciones:', error));
-
-        if (data && Array.isArray(data[propertyName])) {
-            data[propertyName].forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.id;
-                option.text = item.area || item.toner; 
-                select.add(option);
-            });
-        } else {
-            console.error('La propiedad "' + propertyName + '" no es un array en los datos:', data);
-        }
-    }
-
-    async function obtenerNombreSeleccionado(url, selectedId, propertyName) {
-        const data = await fetch(url)
-            .then(res => res.json())
-            .catch(error => {
-                console.error('Error al obtener el nombre del elemento seleccionado:', error);
-                return 'Error';
-            });
-
-        const selectedItem = data[propertyName].find(item => item.id == selectedId);
-        return selectedItem ? selectedItem.area || selectedItem.toner : 'No encontrado';
-    }
-
+ 
     async function mensajeEntrega(mensaje){
         const mensajeDiv = document.createElement('div')
         mensajeDiv.classList.add('mensaje')
